eede8c3f62630fa73c9fe9b35407b39cfb5468b8
kadavr95 <kadavr95@gmail.com>
2017-08-23 19:12:24 +0300

    -

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,65 +1,40 @@
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.category.DefaultCategoryDataset;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-//https://github.com/osobolev/hashtest/blob/master/src/main/java/ru/mirea/hash/BarSample.java
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
- * Created by admin on 10.02.2017.
+ * Created by kadavr95 on 23.08.2017.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         HashMap<String, Integer> wordMap = new HashMap<String, Integer>();
 
-        try (Scanner s = new Scanner(new File("C:\\Users\\kadav\\IdeaProjects\\wordcounter\\src\\main\\java\\gorod.txt"), "UTF-8")) {
+        try (Scanner s = new Scanner(new File("src\\main\\java\\cardslist.txt"), "UTF-8")) {
+            String word="";
             while (s.hasNext()) {
-                String word = s.next().toLowerCase();
-                if (wordMap.get(word) != null) {
-                    wordMap.put(word, wordMap.get(word) + 1);
-                } else {
-                    wordMap.put(word, 1);
+                word = word+s.next()+" ";
+            }
+            Pattern p = Pattern.compile("<a href=\"/cards/(\\d?\\d?\\d?\\d?\\d?\\d?)/.*?aria-label=\"(.*?)\"");
+            Matcher m = p.matcher(word);
+            while (m.find()) {
+                if (wordMap.get(m.group(2)) == null) {
+                    wordMap.put(m.group(2), Integer.parseInt(m.group(1)));
                 }
-
             }
         }
+        try (BufferedReader br = new BufferedReader(new FileReader(new File("src\\main\\java\\cardscolumn.txt")))) {
+            String line="";
+            while (((line = br.readLine()) != null)) {
+                if (wordMap.get(line) != null) {
+                    line=line.replace(line,"<a href=\"https://hsreplay.net/cards/"+wordMap.get(line)+"\">"+line+"</a>");
+                    System.out.println(line);
+                }
+            }
 
-        for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-            System.out.println("Word =  " + entry.getKey() + " Quantity = " + entry.getValue());
         }
-
-        ArrayList<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String,Integer>>(wordMap.entrySet());
-        list.sort((e1,e2)->-e1.getValue().compareTo(e2.getValue()));
-
-        int sum=0;
-
-        for (Map.Entry<String, Integer> entry : list) {
-            System.out.println("Word =  " + entry.getKey() + " Quantity = " + entry.getValue());
-            sum+=entry.getValue();
-        }
-
-        System.out.println("Total quantity: "+sum);
-
-        DefaultCategoryDataset data = new DefaultCategoryDataset();
-        String category = "Words frequency";
-        for (Map.Entry<String, Integer> entry : list) {
-            if (entry.getValue()>=sum/100)
-                data.addValue(entry.getValue(),category,entry.getKey());
-            else
-                break;
-        }
-
-        JFreeChart chart = ChartFactory.createBarChart("Words frequency", "Word", "Quantity", data);
-        BufferedImage image = chart.createBufferedImage(600, 400);
-        File file = new File("chart.png");
-        ImageIO.write(image, "png", file);
-        Desktop.getDesktop().open(file);
     }
 }
