3010af1be713ec47d05ce2d66a1f6b59f43a1f21
kadavr95 <kadavr95@gmail.com>
2017-12-16 21:26:02 +0300

    calculation of cards rating and creation of webtable instead of just converting names to links

---
+++ src/main/java/CardData.java
@@ -0,0 +1,161 @@
+/**
+ * Created by Yaskovich Dmitry on 15/12/2017.
+ */
+public class CardData implements Comparable<CardData> {
+    private String nameEN, nameRU, rarity, gameSet, type;
+    int id;
+    private double popularityStandard, winrateStandard, copiesStandard, ratingStandard,
+            popularityWild, winrateWild, copiesWild, ratingWild, ratingOverall;
+
+    public CardData(String nameEN, int id) {
+        this.nameEN = nameEN;
+        this.id = id;
+    }
+
+    public double getPopularityStandard() {
+        return popularityStandard;
+    }
+
+    public void setPopularityStandard(double popularityStandard) {
+        this.popularityStandard = popularityStandard;
+    }
+
+    public double getWinrateStandard() {
+        return winrateStandard;
+    }
+
+    public void setWinrateStandard(double winrateStandard) {
+        this.winrateStandard = winrateStandard;
+    }
+
+    public double getCopiesStandard() {
+        return copiesStandard;
+    }
+
+    public void setCopiesStandard(double copiesStandard) {
+        this.copiesStandard = copiesStandard;
+    }
+
+    public double getPopularityWild() {
+        return popularityWild;
+    }
+
+    public void setPopularityWild(double popularityWild) {
+        this.popularityWild = popularityWild;
+    }
+
+    public double getWinrateWild() {
+        return winrateWild;
+    }
+
+    public void setWinrateWild(double winrateWild) {
+        this.winrateWild = winrateWild;
+    }
+
+    public double getCopiesWild() {
+        return copiesWild;
+    }
+
+    public void setCopiesWild(double copiesWild) {
+        this.copiesWild = copiesWild;
+    }
+
+    public double getRatingStandard() {
+        return ratingStandard;
+    }
+
+    public void setRatingStandard(double ratingStandard) {
+        this.ratingStandard = ratingStandard;
+    }
+
+    public double getRatingWild() {
+        return ratingWild;
+    }
+
+    public void setRatingWild(double ratingWild) {
+        this.ratingWild = ratingWild;
+    }
+
+    public double getRatingOverall() {
+        return ratingOverall;
+    }
+
+    public void setRatingOverall(double ratingOverall) {
+        this.ratingOverall = ratingOverall;
+    }
+
+    public String getNameEN() {
+        return nameEN;
+    }
+
+    public void setNameEN(String nameEN) {
+        this.nameEN = nameEN;
+    }
+
+    public String getNameRU() {
+        return nameRU;
+    }
+
+    public void setNameRU(String nameRU) {
+        this.nameRU = nameRU;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getGameSet() {
+        return gameSet;
+    }
+
+    public void setGameSet(String gameSet) {
+        this.gameSet = gameSet;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "CardData{" +
+                "nameEN='" + nameEN + '\'' +
+                ", nameRU='" + nameRU + '\'' +
+                ", rarity='" + rarity + '\'' +
+                ", gameSet='" + gameSet + '\'' +
+                ", type='" + type + '\'' +
+                ", id=" + id +
+                ", popularityStandard=" + popularityStandard +
+                ", winrateStandard=" + winrateStandard +
+                ", copiesStandard=" + copiesStandard +
+                ", ratingStandard=" + ratingStandard +
+                ", popularityWild=" + popularityWild +
+                ", winrateWild=" + winrateWild +
+                ", copiesWild=" + copiesWild +
+                ", ratingWild=" + ratingWild +
+                ", ratingOverall=" + ratingOverall +
+                '}';
+    }
+
+    @Override
+    public int compareTo(CardData o) {
+        return (int)(getRatingOverall()*1000000)- (int)(o.getRatingOverall()*1000000);
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import javax.smartcardio.Card;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -5,56 +6,153 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 /**
  * Created by kadavr95 on 23.08.2017.
  */
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        HashMap<String, Integer> wordMap = new HashMap<String, Integer>();
-        HashMap<String, String> langMap = new HashMap<String, String>();
+        HashMap<String, CardData> cardMap = new HashMap<>();
+        int cardsCounter = 0;
 
         try (Scanner s = new Scanner(new File("src\\main\\java\\cardslist.txt"), "UTF-8")) {
             String word = new Scanner(new File("src\\main\\java\\cardslist.txt")).useDelimiter("\\Z").next();
             Pattern p = Pattern.compile("<a href=\"/cards/(\\d?\\d?\\d?\\d?\\d?\\d?)/.*?aria-label=\"(.*?)\"");
             Matcher m = p.matcher(word);
             while (m.find()) {
-                if (wordMap.get(m.group(2)) == null) {
-                    wordMap.put(m.group(2), Integer.parseInt(m.group(1)));
+                if (cardMap.get(m.group(2)) == null) {
+                    cardMap.put(m.group(2), new CardData(m.group(2), Integer.parseInt(m.group(1))));
+                    cardsCounter++;
                 }
             }
         }
+
         try (Scanner s = new Scanner(new File("src\\main\\java\\cardsJSON.txt"), "UTF-8")) {
             String word = new Scanner(new File("src\\main\\java\\cardsJSON.txt")).useDelimiter("\\Z").next();
             Pattern p = Pattern.compile("<Entity CardID=[\\s\\S]*?<enUS>([\\s\\S]*?)</enUS>[\\s\\S]*?<ruRU>([\\s\\S]*?)</ruRU>");
             Matcher m = p.matcher(word);
             while (m.find()) {
-                if (langMap.get(m.group(1)) == null) {
-                    langMap.put(m.group(1), m.group(2));
+                if (cardMap.get(m.group(1)) != null) {
+                    cardMap.get(m.group(1)).setNameRU(m.group(2));
                 }
             }
         }
-        try (BufferedReader br = new BufferedReader(new FileReader(new File("src\\main\\java\\cardscolumn.txt")))) {
-            String line="";
+
+        try (Scanner s = new Scanner(new File("src\\main\\java\\cardsJSON"), "UTF-8")) {
+            String word = new Scanner(new File("src\\main\\java\\cardsJSON")).useDelimiter("\\Z").next();
+            Pattern p = Pattern.compile("\"name\":\"([\\s\\S]*?)\",[\\s\\S]*?\"rarity\":\"([\\s\\S]*?)\",[\\s\\S]*?\"set\":\"([\\s\\S]*?)\",");
+            Matcher m = p.matcher(word);
+            while (m.find()) {
+                if (cardMap.get(m.group(1)) != null) {
+                    cardMap.get(m.group(1)).setRarity(m.group(2));
+                    cardMap.get(m.group(1)).setGameSet(m.group(3));
+                }
+            }
+        }
+
+        String[][] rawData = new String[10][cardsCounter + 2];
+
+        try (BufferedReader br = new BufferedReader(new FileReader(new File("src\\main\\java\\cardsStandard")))) {
+            String line = "";
+            cardsCounter = 0;
+            int counter = 0;
+            boolean finNames = false;
             while (((line = br.readLine()) != null)) {
-                if (wordMap.get(line) != null) {
-                    line=line.replace(line,"<a href=\"https://hsreplay.net/cards/"+wordMap.get(line)+"\">"+line+"</a>");
-                    System.out.println(line);
+                counter++;
+                if (finNames == false) {
+                    cardsCounter++;
+                    rawData[1][cardsCounter] = line;
+                    if (line.matches("\\d?\\d?\\d?.\\d?\\d?\\d?\\d?\\d?\\d?\\d?\\d?")) {
+                        rawData[2][1] = line;
+                        finNames = true;
+                    }
+                } else {
+                    rawData[(counter - cardsCounter) % 6 + 2][(counter - cardsCounter) / 6 + 1] = line;
                 }
             }
+            for (int i = 1; i < cardsCounter; i++) {
+                cardMap.get(rawData[1][i]).setPopularityStandard(Double.parseDouble(rawData[2][i]));
+                cardMap.get(rawData[1][i]).setCopiesStandard(Double.parseDouble(rawData[3][i]));
+                cardMap.get(rawData[1][i]).setWinrateStandard(Double.parseDouble(rawData[4][i]
+                        .replace("-","0")));
+//                System.out.println(rawData[1][i] + " , " + rawData[2][i] + " , " + rawData[3][i] + " , " +
+//                        rawData[4][i] + " , " + rawData[5][i] + " , " + rawData[6][i] + " , " + rawData[7][i]);
+            }
         }
-        try (BufferedReader br = new BufferedReader(new FileReader(new File("src\\main\\java\\cardscolumn.txt")))) {
-            String line="";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(new File("src\\main\\java\\cardsWild")))) {
+            String line = "";
+            cardsCounter = 0;
+            int counter = 0;
+            boolean finNames = false;
             while (((line = br.readLine()) != null)) {
-                if (wordMap.get(line) != null) {
-                    line=line.replace(line,"<a href=\"https://hsreplay.net/cards/"+wordMap.get(line)+"\">"+langMap.get(line)+"</a>");
-                    System.out.println(line);
+                counter++;
+                if (finNames == false) {
+                    cardsCounter++;
+                    rawData[1][cardsCounter] = line;
+                    if (line.matches("\\d?\\d?\\d?.\\d?\\d?\\d?\\d?\\d?\\d?\\d?\\d?")) {
+                        rawData[2][1] = line;
+                        finNames = true;
+                    }
+                } else {
+                    rawData[(counter - cardsCounter) % 6 + 2][(counter - cardsCounter) / 6 + 1] = line;
                 }
             }
+            for (int i = 1; i < cardsCounter; i++) {
+                cardMap.get(rawData[1][i]).setPopularityWild(Double.parseDouble(rawData[2][i]));
+                cardMap.get(rawData[1][i]).setCopiesWild(Double.parseDouble(rawData[3][i]));
+                cardMap.get(rawData[1][i]).setWinrateWild(Double.parseDouble(rawData[4][i].replace
+                        ("-","0")));
+//                System.out.println(rawData[1][i] + " , " + rawData[2][i] + " , " + rawData[3][i] + " , " +
+//                        rawData[4][i] + " , " + rawData[5][i] + " , " + rawData[6][i] + " , " + rawData[7][i]);
+            }
         }
+
+        for (HashMap.Entry<String, CardData> entry : cardMap.entrySet()) {
+            cardMap.get(entry.getKey()).setRatingStandard(entry.getValue().getPopularityStandard() * entry.getValue()
+                    .getWinrateStandard() * 2 * 2030 / 30);
+            cardMap.get(entry.getKey()).setRatingWild(entry.getValue().getPopularityWild() * entry.getValue()
+                    .getWinrateWild() * 2 * 2711 / 30);
+            cardMap.get(entry.getKey()).setRatingOverall(entry.getValue().getRatingStandard() * 0.9 + entry.getValue().getRatingWild() * 0.1);
+        }
+
+        HashMap<String, CardData> cardMap2 = new HashMap<>();
+        cardMap2 = sortByValue(cardMap);
+        for (HashMap.Entry<String, CardData> entry : cardMap2.entrySet()) {
+            System.out.println("<tr><td><a href=\"https://hsreplay.net/cards/" + entry.getValue().getId() + "\">" +
+                    entry.getValue().getNameEN() + "</a></td><td>"+((entry.getValue().getRarity().equals("LEGENDARY")
+            ) ? "★" : "")+"</td><td>" + (entry.getValue().getCopiesStandard() * 0.9 + entry.getValue().getCopiesWild
+                    () * 0.1) + "</td><td>" + entry.getValue().getRatingStandard() + "</td><td>" + entry.getValue()
+                    .getRatingWild() + "</td><td>" + entry.getValue().getRatingOverall() +
+                    "</td><td>"+entry.getValue().getRarity()+"</td><td>"+entry.getValue().getGameSet()+"</td></tr>");
+            //System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+        }
+        System.out.println("\n\n\n\n\n");
+        for (HashMap.Entry<String, CardData> entry : cardMap2.entrySet()) {
+            System.out.println("<tr><td><a href=\"https://hsreplay.net/cards/" + entry.getValue().getId() + "\">" +
+                    entry.getValue().getNameRU() + "</a></td><td>"+((entry.getValue().getRarity().equals("LEGENDARY")
+            ) ? "★" : "")+"</td><td>" + (entry.getValue().getCopiesStandard() * 0.9 + entry.getValue().getCopiesWild
+                    () * 0.1) + "</td><td>" + entry.getValue().getRatingStandard() + "</td><td>" + entry.getValue()
+                    .getRatingWild() + "</td><td>" + entry.getValue().getRatingOverall() +
+                    "</td><td>"+entry.getValue().getRarity()+"</td><td>"+entry.getValue().getGameSet()+"</td></tr>");
+            //System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+        }
+    }
+
+    public static <K, V extends Comparable<? super V>> HashMap<K, V> sortByValue(HashMap<K, V> map) {
+        return map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
     }
 }
